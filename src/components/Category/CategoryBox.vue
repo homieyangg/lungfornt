11b4accea9345cc169d8f3a1dcc81e743930bd5e
homieyang <homieyang@outlook.com>
2022-06-29 23:49:18 +0800

    add in 06/29

---
+++ src/components/Category/CategoryBox.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="card w-100 h-100" style="margin-top: 10px">
+        <div class="embed-responsive embed-responsive-16by9">
+            <img :src="category.imageUrl" class="card-img-top embed-responsive-item" alt="...">
+        </div>
+        <div class="card-body">
+            <h5 class="card-title">{{category.categoryName}}</h5>
+            <p class="card-text">{{category.description}}</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
+    </div>
+</template>
+<script>
+    
+    export default {
+        name: "CategoryBox",
+        props:["category"],
+        methods:{},
+    };
+
+</script>
