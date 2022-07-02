116592f5fdad71a29d1638a392e0baead6de8126
homieyang <homieyang@outlook.com>
2022-07-02 17:45:45 +0800

    update

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,43 +1,51 @@
 <template>
     <!-- Hero/Intro Slider Start -->
-    <div id="carouselExampleDark" class="carousel carousel-dark slide" data-bs-ride="carousel">
-  <div class="carousel-indicators">
-    <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-    <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="1" aria-label="Slide 2"></button>
-    <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="2" aria-label="Slide 3"></button>
-  </div>
-  <div class="carousel-inner">
-    <div class="carousel-item active" data-bs-interval="10000">
-      <img src="assets/images/slider/forslide1.jpg" class="d-block w-100" alt="">
-      <div class="carousel-caption d-none d-md-block">
-        <h5>First slide label</h5>
-        <p>Some representative placeholder content for the first slide.</p>
-      </div>
+    <div class="section">
+        <div class="hero-slider swiper-container">
+            <div class="swiper-wrapper">
+
+                <div class="hero-slide-item swiper-slide">
+                    <div class="hero-slide-bg">
+                        <img src="assets/images/slider/cat.jpg" alt="Slider Image" />
+                    </div>
+                    <div class="container">
+                        <div class="hero-slide-content text-start">
+                            <h5 class="sub-title">We keep pets for pleasure.</h5>
+                            <h2 class="title m-0">Vitamins For all Pets</h2>
+                            <p class="ms-0">We know your concerns when you are looking for a chewing treat for your dog.
+                            </p>
+                            <a href="shop.html" class="btn btn-dark btn-hover-primary">Shop Now</a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="hero-slide-item swiper-slide">
+                    <div class="hero-slide-bg">
+                        <img src="assets/images/slider/dog.jpg" alt="Slider Image" />
+                    </div>
+                    <div class="container">
+                        <div class="hero-slide-content text-center text-md-end">
+                            <h5 class="sub-title">We keep pets for pleasure.</h5>
+                            <h2 class="title m-0">Vitamins For all Pets</h2>
+                            <p>We know your concerns when you are looking for a chewing treat for your dog.</p>
+                            <a href="shop.html" class="btn btn-dark btn-hover-primary">Shop Now</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <!-- Swiper Pagination Start -->
+            <div class="swiper-pagination d-lg-none"></div>
+            <!-- Swiper Pagination End -->
+
+            <!-- Swiper Navigation Start -->
+            <div class="home-slider-prev swiper-button-prev main-slider-nav d-lg-flex d-none"><i
+                    class="icon-arrow-left"></i></div>
+            <div class="home-slider-next swiper-button-next main-slider-nav d-lg-flex d-none"><i
+                    class="icon-arrow-right"></i></div>
+            <!-- Swiper Navigation End -->
+        </div>
     </div>
-    <div class="carousel-item" data-bs-interval="2000">
-      <img src="assets/images/slider/forslide2.jpg" class="d-block w-100" alt="">
-      <div class="carousel-caption d-none d-md-block">
-        <h5>Second slide label</h5>
-        <p>Some representative placeholder content for the second slide.</p>
-      </div>
-    </div>
-    <div class="carousel-item">
-      <img src="assets/images/slider/forslide3.jpg" class="d-block w-100" alt="">
-      <div class="carousel-caption d-none d-md-block">
-        <h5>Third slide label</h5>
-        <p>Some representative placeholder content for the third slide.</p>
-      </div>
-    </div>
-  </div>
-  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Previous</span>
-  </button>
-  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Next</span>
-  </button>
-</div>
     <!-- Hero/Intro Slider End -->
 
     <div class="section section-padding">
